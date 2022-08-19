6c8b9d42a42179a8dedfea1222aa7d6f88b3b524
caio17harved <caio.croccia@gmail.com>
2022-08-19 19:18:20 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,10 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+console.log("ola mundo");
+
+//laços
+for(let i = 1; i <=  10; i++){
+  console.log(i);
+}
